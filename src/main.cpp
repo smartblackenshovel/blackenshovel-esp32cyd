b4b234fb24b3a53e3f2512fd889f9882cd28ca42
Lauro Cesar <lauroandradearaujo@gmail.com>
2025-11-27 21:56:31 +0100

    initial code

--- src/main.cpp
+++ src/main.cpp
@@ -2,17 +2,14 @@
 #include <TFT_eSPI.h>
 #include <XPT2046_Touchscreen.h>
 #include <WiFi.h>
-#include <WiFiClientSecure.h>
 #include <HTTPClient.h>
-#include <PNGdec.h>
-#include <TFT_eFEX.h>
 #include <SD.h>
-#include "DisplayHandler.h"
 #include "HTTPHandler.h"
 #include <vector>
 #include <time.h>
 #include <ArduinoJson.h>
 #include "JsonUtils.h"
+#include <lvgl.h>
 
 
 #define XPT2046_IRQ 36
@@ -27,12 +24,14 @@
 
 #define SD_CS 4
 
+#define HTTP_ACTIVE 0
+
+#define DRAW_BUF_SIZE (SCREEN_WIDTH * SCREEN_HEIGHT / 10 * (LV_COLOR_DEPTH / 8))
+uint32_t draw_buf[DRAW_BUF_SIZE / 4];
+
 TFT_eSPI tft = TFT_eSPI();
 
-PNG png;
-
 WiFiClientSecure securedClient;
-FileFetcher fileFetcher(securedClient);
 
 SPIClass touchscreenSPI = SPIClass(VSPI);
 XPT2046_Touchscreen touchscreen(XPT2046_CS, XPT2046_IRQ);
@@ -54,7 +53,7 @@ char endpointSessions[] = "/sessions";
 char endpointSpotLogs[] = "/spot_logs";
 char endpointSessionLogs[] = "/session_logs";
 char mapEndpoint[] = "/map?lat=47.2229&lon=8.8169";
-char imageFileUri[] = IMAGE_NAME;
+
 String shovelSerialNumber = "BS-#13823429-02";
 String shovelId;
 String sessionId;
@@ -74,6 +73,119 @@ void drawMenu(const std::vector<String>& items, int itemHeight = 40, uint16_t bg
         tft.setTextColor(textColor);
         tft.drawString(items[i], tft.width()/2, y + itemHeight/2);
     }
+}
+
+// Get the Touchscreen data
+void touchscreen_read(lv_indev_t * indev, lv_indev_data_t * data) {
+  // Checks if Touchscreen was touched, and prints X, Y and Pressure (Z)
+  if(touchscreen.tirqTouched() && touchscreen.touched()) {
+    // Get Touchscreen points
+    TS_Point p = touchscreen.getPoint();
+    // Calibrate Touchscreen points with map function to the correct width and height
+    x = map(p.x, 200, 3700, 0, SCREEN_WIDTH);
+    y = map(p.y, 240, 3800, 0, SCREEN_HEIGHT);
+    z = p.z;
+
+    data->state = LV_INDEV_STATE_PRESSED;
+
+    // Set the coordinates
+    data->point.x = x;
+    data->point.y = y;
+
+    // Print Touchscreen info about X, Y and Pressure (Z) on the Serial Monitor
+    Serial.print("X = ");
+    Serial.print(x);
+    Serial.print(" | Y = ");
+    Serial.print(y);
+    Serial.print(" | Pressure = ");
+    Serial.print(z);
+    Serial.println();
+  }
+  else {
+    data->state = LV_INDEV_STATE_RELEASED;
+  }
+}
+
+// If logging is enabled, it will inform the user about what is happening in the library
+void log_print(lv_log_level_t level, const char * buf) {
+  LV_UNUSED(level);
+  Serial.println(buf);
+  Serial.flush();
+}
+
+int btn1_count = 0;
+// Callback that is triggered when btn1 is clicked
+static void event_handler_btn1(lv_event_t * e) {
+  lv_event_code_t code = lv_event_get_code(e);
+  if(code == LV_EVENT_CLICKED) {
+    btn1_count++;
+    LV_LOG_USER("Button clicked %d%", (int)btn1_count);
+  }
+}
+
+// Callback that is triggered when btn2 is clicked/toggled
+static void event_handler_btn2(lv_event_t * e) {
+  lv_event_code_t code = lv_event_get_code(e);
+  lv_obj_t * obj = (lv_obj_t*) lv_event_get_target(e);
+  if(code == LV_EVENT_VALUE_CHANGED) {
+    LV_UNUSED(obj);
+    LV_LOG_USER("Toggled %s", lv_obj_has_state(obj, LV_STATE_CHECKED) ? "on" : "off");
+  }
+}
+
+static lv_obj_t * slider_label;
+// Callback that prints the current slider value on the TFT display and Serial Monitor for debugging purposes
+static void slider_event_callback(lv_event_t * e) {
+  lv_obj_t * slider = (lv_obj_t*) lv_event_get_target(e);
+  char buf[8];
+  lv_snprintf(buf, sizeof(buf), "%d%%", (int)lv_slider_get_value(slider));
+  lv_label_set_text(slider_label, buf);
+  lv_obj_align_to(slider_label, slider, LV_ALIGN_OUT_BOTTOM_MID, 0, 10);
+  LV_LOG_USER("Slider changed to %d%%", (int)lv_slider_get_value(slider));
+}
+
+void lv_create_main_gui(void) {
+  // Create a text label aligned center on top ("Hello, world!")
+  lv_obj_t * text_label = lv_label_create(lv_screen_active());
+  lv_label_set_long_mode(text_label, LV_LABEL_LONG_WRAP);    // Breaks the long lines
+  lv_label_set_text(text_label, "Hello, world!");
+  lv_obj_set_width(text_label, 150);    // Set smaller width to make the lines wrap
+  lv_obj_set_style_text_align(text_label, LV_TEXT_ALIGN_CENTER, 0);
+  lv_obj_align(text_label, LV_ALIGN_CENTER, 0, -90);
+
+  lv_obj_t * btn_label;
+  // Create a Button (btn1)
+  lv_obj_t * btn1 = lv_button_create(lv_screen_active());
+  lv_obj_add_event_cb(btn1, event_handler_btn1, LV_EVENT_ALL, NULL);
+  lv_obj_align(btn1, LV_ALIGN_CENTER, 0, -50);
+  lv_obj_remove_flag(btn1, LV_OBJ_FLAG_PRESS_LOCK);
+
+  btn_label = lv_label_create(btn1);
+  lv_label_set_text(btn_label, "Button");
+  lv_obj_center(btn_label);
+
+  // Create a Toggle button (btn2)
+  lv_obj_t * btn2 = lv_button_create(lv_screen_active());
+  lv_obj_add_event_cb(btn2, event_handler_btn2, LV_EVENT_ALL, NULL);
+  lv_obj_align(btn2, LV_ALIGN_CENTER, 0, 10);
+  lv_obj_add_flag(btn2, LV_OBJ_FLAG_CHECKABLE);
+  lv_obj_set_height(btn2, LV_SIZE_CONTENT);
+
+  btn_label = lv_label_create(btn2);
+  lv_label_set_text(btn_label, "Toggle");
+  lv_obj_center(btn_label);
+  
+  // Create a slider aligned in the center bottom of the TFT display
+  lv_obj_t * slider = lv_slider_create(lv_screen_active());
+  lv_obj_align(slider, LV_ALIGN_CENTER, 0, 60);
+  lv_obj_add_event_cb(slider, slider_event_callback, LV_EVENT_VALUE_CHANGED, NULL);
+  lv_slider_set_range(slider, 0, 100);
+  lv_obj_set_style_anim_duration(slider, 2000, 0);
+
+  // Create a label below the slider to display the current slider value
+  slider_label = lv_label_create(lv_screen_active());
+  lv_label_set_text(slider_label, "0%");
+  lv_obj_align_to(slider_label, slider, LV_ALIGN_OUT_BOTTOM_MID, 0, 10);
 }
 
 String currentTime() {
@@ -98,9 +210,29 @@ void setup() {
 
   touchscreenSPI.begin(XPT2046_CLK, XPT2046_MISO, XPT2046_MOSI, XPT2046_CS);
   touchscreen.begin(touchscreenSPI);
-  touchscreen.setRotation(1);
+  touchscreen.setRotation(0);
+
   tft.init();
-  tft.setRotation(1);
+  tft.setRotation(0);
+  tft.fillScreen(TFT_BLACK);
+
+  //-------------------------------------------------------------------------------------
+
+  Serial.println("Initializing LVGL Library...");
+  String LVGL_Arduino = String("LVGL Library Version: ") + lv_version_major() + "." + lv_version_minor() + "." + lv_version_patch();
+  Serial.println(LVGL_Arduino);
+  lv_init();
+  lv_log_register_print_cb(log_print);
+
+  lv_display_t* disp;
+  disp = lv_tft_espi_create(SCREEN_WIDTH, SCREEN_HEIGHT, draw_buf, sizeof(draw_buf));
+  lv_display_set_rotation(disp, LV_DISPLAY_ROTATION_270);
+
+  lv_indev_t* indev = lv_indev_create();
+  lv_indev_set_type(indev, LV_INDEV_TYPE_POINTER);
+  lv_indev_set_read_cb(indev, touchscreen_read);
+
+  lv_create_main_gui();
 
   //-------------------------------------------------------------------------------------
 
@@ -142,7 +274,7 @@ void setup() {
   Serial.println("SD card initialized.");
 
   //-------------------------------------------------------------------------------------
-
+  #if HTTP_ACTIVE
   Serial.println("Fetching Shovel...");
   HTTPResponse shovelsResponse = httpHandler.get(endpointShovels, {
     {"serial_number", shovelSerialNumber}
@@ -176,10 +308,11 @@ void setup() {
     Serial.printf("Failed to fetch organization. Status code: %d\n", organizationsResponse.getStatusCode());
     return;
   }
+  #endif
 }
 
 void loop() {
-
+  #if HTTP_ACTIVE
   if (!selectedUser) {
 
   // SELECT USER -------------------------------------------------------------------------
@@ -263,8 +396,11 @@ void loop() {
   }
 
   delay(10000);
-  return;
+  #endif
 
+  lv_task_handler();
+  lv_tick_inc(5);
+  delay(5);
   // Get Coordinates of destination spot
 
   // Get Accel Data and Gyro data
